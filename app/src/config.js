681df06a66c2fabac8b58dc5cc9f3d67423a028b
shubham1172 <shubhamsharma1172@gmail.com>
2017-07-30 13:24:31 +0530

    Modified routing and getUploaded

--- app/src/config.js
+++ app/src/config.js
@@ -9,7 +9,7 @@
  //PORT NUMBER
  config.PORT_NUMBER = 8080;
  //PERMITTED_URLS for auth
- config.PERMITTED_URLS = ["/", "/register", "/logout", "/login", "/check-eligible",
+ config.PERMITTED_URLS = ["/", "/css/index.css", "/js/shared/signin.js","/register", "/logout", "/login", "/check-eligible",
                           "/get-streams", "/get-colleges", "/check-username"];
  //HTTP_CODES used in app
  config.HTTP_CODES = {OK: 200, BAD_REQUEST: 400, FORBIDDEN: 403, SERVER_ERROR: 500};
