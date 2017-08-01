19b0c33bf1982afc9b91e6ff29e9ed0cfdcd462a
shubham1172 <shubhamsharma1172@gmail.com>
2017-08-01 16:45:39 +0530

    Changes made to modal

--- app/src/public/js/controllers/searchController.js
+++ app/src/public/js/controllers/searchController.js
@@ -3,7 +3,7 @@
  * search-books
  */
 
-app.controller('searchBook', function($scope, $http){ 
+app.controller('searchBook', function($scope, $http){
     $('#search-box').keypress(function(e){
         if(e.which == 13){
             console.log('pressed enter');
@@ -12,9 +12,55 @@ app.controller('searchBook', function($scope, $http){
             $scope.books = response;
             console.log(response);
             });
-
             var curr = 0;
             var i = setTimeout(function(){
+
+              $('.book').on("click",function(){
+                  console.log('clicked');
+                  var bookId = $(this).children('.bookId').html();
+                  $('#book-modal').modal('open');
+                  var bookReq = new XMLHttpRequest();
+                  bookReq.onload = function(){
+                      if(bookReq.readyState = XMLHttpRequest.DONE){
+                          if(bookReq.status === 200||bookReq.status === 304){
+                              var book = JSON.parse(bookReq.responseText);
+                              console.log(book);
+                              $("#book-title-modal").html(book.name);
+                              $("#book-author-modal").html('By '+book.author);
+                              $("#book-publisher-modal").html('Published by '+book.publisher);
+                              $("#book-memo-modal").html("About book: "+book.memo);
+                              $("#book-user-modal").html("Name: "+book.user);
+                              $("#book-contact-modal").html("Contact: "+book.mobile);
+                              $("#book-year-modal").html("Year: "+book.year);
+                              $("#book-stream-modal").html("Stream: "+book.type+", "+book.stream);
+                              $("#cost").html("₹"+book.price);
+                              $("#book-condition-modal").html(book.condition);
+                              var photoid = book.photo_id;
+                              //Photos
+                              var photos = new XMLHttpRequest();
+                              photos.onload = function(){
+                                  if(photos.readyState = XMLHttpRequest.DONE){
+                                      if(photos.status===200){
+                                          var photo = JSON.parse(photos.responseText);
+                                          $("#book-photo1-modal").attr('src',photo.image_1);
+                                           $("#book-photo2-modal").attr('src',photo.image_2);
+                                      }else{
+                                          console.log(photos.responseText);
+                                      }
+                                  }
+                              }
+                              photos.open('GET','http://localhost:8080/get-photos?id='+photoid,true);
+                              photos.send(null);
+
+                          }else{
+                              console.log(bookReq.responseText);
+                          }
+                      }
+                  }
+                  console.log(bookId);
+                  bookReq.open('GET','http://localhost:8080/get-book?id='+bookId,true);
+                  bookReq.send(null);
+                });
                 var collection = document.getElementsByClassName('card-image');
                 iterate(collection);
             },500);
